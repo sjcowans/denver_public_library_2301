9aea3b32da99f4b1e2ec7668775c0f4f17f85a70
Sean Cowans <zedesia@yahoo.com>
2023-02-24 09:57:49 -0700

    Add test for adding authors

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -31,4 +31,28 @@ RSpec.describe Library do
     expect(library.authors).to eq([])
     end
   end
+
+  describe 'can add author' do
+    it 'can list authors' do
+    library = Library.new("Seanbrary")
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    sean_cowans = Author.new({first_name: "Sean", last_name: "Cowans"})
+    library.add_author(charlotte_bronte)
+    library.add_author(sean_cowans)
+
+    expect(library.authors).to eq([charlotte_bronte, sean_cowans])
+    end
+
+    it 'can add authors books' do
+    library = Library.new("Seanbrary")
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    villette = charlotte_bronte.write("Villette", "1853")
+    sean_cowans = Author.new({first_name: "Sean", last_name: "Cowans"})
+    code_like_a_pro = sean_cowans.write("Code Like a Pro", "2023")
+    library.add_author(charlotte_bronte)
+    library.add_author(sean_cowans)
+
+    expect(library.books).to eq([villette, code_like_a_pro])
+    end
+  end
 end
