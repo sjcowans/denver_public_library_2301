075b483931ac5605620781b5048539de1b3b447e
Sean Cowans <zedesia@yahoo.com>
2023-02-24 09:07:54 -0700

    Create book spec

---
+++ spec/book_spec.rb
@@ -0,0 +1,33 @@
+require 'rspec'
+require './lib/book'
+
+RSpec.describe Book do
+  describe '#initialize' do
+    it 'can initialize' do
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    expect(book).to be_a(Book)
+    end
+  end
+
+  describe 'has proper attributes' do
+    it 'has title' do
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    expect(book.title).to eq("To Kill a Mockingbird")
+    end
+
+    it 'has author' do
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    expect (book.author).to eq("Harper Lee")
+    end
+
+    it 'has publication year' do
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    expect(book.publication_year).to eq("1960")
+    end
+  end
+end
+
