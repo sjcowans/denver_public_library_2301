947723d7686ac2f4ec7b75ab9a045e8d9d6caeba
Sean Cowans <zedesia@yahoo.com>
2023-02-24 09:15:40 -0700

    Create book class

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,18 @@
 class Book
-  
+  attr_reader :title
+
+  def initialize(information)
+    @first = information[:author_first_name]
+    @last = information[:author_last_name]
+    @title = information[:title]
+    @date = information[:publication_date]
+  end
+
+  def author
+    @first + (" ") + @last
+  end
+
+  def publication_year
+    @date.split(" ").last
+  end
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe Book do
     it 'has author' do
     book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
 
-    expect (book.author).to eq("Harper Lee")
+    expect(book.author).to eq("Harper Lee")
     end
 
     it 'has publication year' do
